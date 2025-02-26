148ee902d8c87931dc7ec0bb31f9913bee5cc7dc
yakoalex <yakoales@gmail.com>
2025-02-26 20:39:24 +0200

    created Exceptions

--- src/main/java/experiments/Exeptions.java
+++ src/main/java/experiments/Exeptions.java
@@ -1,2 +1,34 @@
-package experiments;public class Exeptions {
+package experiments;
+
+public class Exeptions {
+    public static void main(String[] args) {
+        String[] strArray={"str1","str2"}
+
+        System.out.println("============");
+        pause();
+        System.out.println("Ura");
+
+    }
+
+    private static void ucheckedEsxception(String[] strArray){
+        try{
+        for ( i=0; i<5; i ++)
+         {
+            System.out.println(strArray[i]);
+
+        }catch(Exception e){
+            e.getSrackTraece();
+                System.out.println("");
+            }
+    }
+    private static void pause()  {
+        try {
+            checkedExeptions();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    private static  void  checkedExeptions() throws InterruptedException {
+        Thread.sleep(3000);
+    }
 }
